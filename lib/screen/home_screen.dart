80194dbbb3df5abfdb26d176db049da7cca50fc9
Santo <santonurul1@gmail.com>
2025-05-20 23:34:49 +0600

    code-add

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -15,20 +15,84 @@ class _HomePageState extends State<HomePage> {
 
   List<Map<String, String>> contacts = [];
 
-  void addContact() {
-    String name = nameController.text.trim();
-    String number = numberController.text.trim();
 
-    if (name.isNotEmpty && number.isNotEmpty) {
-      setState(() {
-        contacts.add({'name': name, 'number': number});
-      });
-      nameController.clear();
-      numberController.clear();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contact List"),
+
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              controller: numberController,
+              keyboardType: TextInputType.name,
+              decoration: const InputDecoration(
+                hintText: 'Name ',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            TextField(
+              controller: numberController,
+              keyboardType: TextInputType.phone,
+              decoration: const InputDecoration(
+                hintText: 'Number',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: addContactList,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueGrey,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 50,right: 50,top: 10,bottom: 10),
+                child: const Text('Add  ',style: TextStyle(
+                  color: Colors.white
+                ),),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  final contact = contacts[index];
+                  return GestureDetector(
+                    onLongPress: () => confirmDeleteDialog(index),
+                    child: ListTile(
+                      leading: const Icon(Icons.person),
+                      title: Text(
+                        contact['name']!,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                      subtitle: Text(contact['number']!),
+                      trailing: const Icon(Icons.phone, color: Colors.blue),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  void confirmDelete(int index) {
+  void confirmDeleteDialog(int index) {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -53,64 +117,17 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Contact List"),
-        backgroundColor: Colors.blueGrey,
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                hintText: 'Name',
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: numberController,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                hintText: 'Number',
-              ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: addContact,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey,
-              ),
-              child: const Text('Add'),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: contacts.length,
-                itemBuilder: (context, index) {
-                  final contact = contacts[index];
-                  return GestureDetector(
-                    onLongPress: () => confirmDelete(index),
-                    child: ListTile(
-                      leading: const Icon(Icons.person),
-                      title: Text(
-                        contact['name']!,
-                        style: const TextStyle(color: Colors.red),
-                      ),
-                      subtitle: Text(contact['number']!),
-                      trailing: const Icon(Icons.phone, color: Colors.blue),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  void addContactList() {
+    String name = nameController.text.trim();
+    String number = numberController.text.trim();
+
+    if (name.isNotEmpty && number.isNotEmpty) {
+      setState(() {
+        contacts.add({'name': name, 'number': number});
+      });
+      nameController.clear();
+      numberController.clear();
+    }
   }
+
 }
